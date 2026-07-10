1aa31fd2cb7995c5f7474c2d095c57775be48936
Juana Hernández cortez <csarielscontacto@gmail.com>
2026-07-09 18:51:04 -0600

    Create config.js

---
+++ config.js
@@ -0,0 +1,7 @@
+// config.js — Proyecto: trytiktok
+window.CONFIG = {
+  SUPABASE_URL: "https://kbrlmeylciwocsxndtvv.supabase.co",
+  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImticmxtZXlsY2l3b2NzeG5kdHZ2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODM2MzUzNTMsImV4cCI6MjA5OTIxMTM1M30.OwWk3okFUtUAmidKfcJ4Ldf7qRXr201dkwLkWpIzMFU",
+  PROYECTO_NOMBRE: "trytiktok",
+  REGION: "us-east-2"
+};
